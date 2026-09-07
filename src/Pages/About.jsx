f98a321ee51c4fec1f2dd8d43736cc603236dcae
Prabesh Gyawali <prabeshgyawali928@gmail.com>
2026-09-07 01:06:30 -0400

    change in About route

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import photo1 from '../assets/'
+import photo1 from '../assets/me.jpg'
 
 const About = () => {
   return (
-    <div className='h-full w-full'>
+    <div className='h-[100vh] w-[100vw] bg-black'>
+
+        <div className='h-[40%] w-[25%] absolute left-20 top-50'>
+            <img src={photo1} />
+            <h1>okey</h1>
+
+            
+        </div>
         
         
     </div>
