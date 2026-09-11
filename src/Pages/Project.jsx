4903b3a3f4d648ea3e2cf1f117b9c83d8e0ab98e
Prabesh Gyawali <prabeshgyawali928@gmail.com>
2026-09-11 12:48:43 -0400

    adding project layout

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,53 +1,55 @@
-import React , { useRef } from 'react'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import Workcard from '../components/Workcard'
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Work1 from '../assets/work1.webp'
-import Work2 from '../assets/contact.jpg'
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import Workcard from "../components/Workcard";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import Work1 from "../assets/work1.webp";
+import Work2 from "../assets/contact.jpg";
+
 const data1 = "Work Title";
-const data2 = "Web Socket web game"
+const data2 = "Web Socket web game";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Project = () => {
   const container = useRef(null);
-useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: container.current,
-        start: 'top top',
-        end: '+=200%',
-        pin: true,
-        scrub: true,
-      }
-    });
 
-    // 1. First, animate the inner child elements while the card is pinned
-    tl.to('.first-div .smallimg', {
-        scale: 0.85,
-        opacity: 0.3,
-        ease: 'none',
-        pin:true,
-      })
-
-      // 2. Then, slide the parent card away afterward
-      .to('.first-div', {
-        y: '-100%',
-        ease: 'none'
-      });
-
-  }, { scope: container });
+  const work1Ref = useRef(null);
+  const work2Ref = useRef(null);
 
   return (
-    <div ref={container} className=' h-[100vh] w-[100vw] bg-black'>
-<Workcard tittle={data1} imgpath={Work1} className ="first-div absolute inset-0 z-30" />
-<Workcard tittle={data2} imgpath={Work2} className =" inset-0 absolute z-20 " />
-     
-        
-        
-    </div>
-  )
-}
+    <div
+      ref={container}
+      className="relative h-[200vh] w-full bg-black"
+    >
+      <div className="sticky top-0 h-screen w-full overflow-hidden">
 
-export default Project
+        {/* Work 1 */}
+        <div
+          ref={work1Ref}
+          className="absolute inset-0"
+        >
+          <Workcard
+            tittle={data1}
+            imgpath={Work1}
+          />
+        </div>
+
+        {/* Work 2 */}
+        <div
+          ref={work2Ref}
+          className="absolute inset-0 opacity-0"
+        >
+          <Workcard
+            tittle={data2}
+            imgpath={Work2}
+          />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Workcard.jsx
+++ src/components/Workcard.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React from 'react';
 
-
-const Workcard = ({tittle, imgpath , className}) => {
+const Workcard = ({ tittle, imgpath, className }) => {
   return (
-          <div className={`h-[100vh] w-[100vw]  ${className || ''}`}>
+    <div
+      className={`relative h-[100vh] w-[100vw] overflow-hidden ${className || ''}`}
+    >
 
-           
-       <img src={imgpath} className='h-full w-full object-cover' />
-       <div className='h-[57%] w-[50%] absolute left-[40%] top-1/2 -translate-x-1/2 -translate-y-1/2  flex justify-around items-center '> 
-       <div className="text h-full w-[40%] ">
-        <div className='text-white flex flex-col justify-center h-full w-full'>
-          <h1 className='text-5xl'>{tittle}</h1>
-          <p>Feature all projects.</p>
+      {/* Full Background Image */}
+      <img
+        src={imgpath}
+        className='absolute inset-0 h-full w-full object-cover'
+      />
+
+      {/* Center Content */}
+      <div className='absolute left-1/2 top-1/2 h-[57%] w-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-around items-center'>
+
+        {/* Text */}
+        <div className="text h-full w-[40%]">
+          <div className='text-white flex flex-col justify-center h-full w-full'>
+            <h1 className='text-5xl'>{tittle}</h1>
+            <p>Feature all projects.</p>
+          </div>
         </div>
-       </div>
-       <div className="smallimg subimg h-full w-[40%] ">
-         <img src={imgpath} className='h-full w-full object-cover' />
-       </div>
+
+        {/* Small Image */}
+        <div className="smallimg subimg h-full w-[40%]">
+          <img
+            src={imgpath}
+            className='h-full w-full object-cover'
+          />
         </div>
 
       </div>
-  )
+
+    </div>
+  );
 }
 
-export default Workcard
+export default Workcard;
